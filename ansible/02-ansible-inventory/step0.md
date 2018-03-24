5f24259e145c00fd18e1071519f6f6da2bfd94d1
James B. Matthews <james30152@gmail.com>
2018-03-24 18:54:13 -0400

    add

--- ansible/02-ansible-inventory/step0.md
+++ ansible/02-ansible-inventory/step0.md
@@ -11,3 +11,14 @@ apt-get install -y ansible
 
 
 - create inventory "myhosts" (copy from previous model)
+
+Creating an inventory
+In this tutorial you have access to one host named host01. To let Ansible know about it, put its hostname in an inventory file. Let's call our inventory myhosts, with one group called group1.
+
+echo "[group1]" > myhosts
+
+Then add the host to the group:
+
+echo "host01 ansible_ssh_user=ubuntu" >> myhosts
+
+Here we're also passing the username to use for SSH access, as an inventory parameter.
--- ansible/03-ansible-first-playbook/env-init.sh
+++ ansible/03-ansible-first-playbook/env-init.sh
@@ -1,8 +1,5 @@
-apt-get -y update
-apt-get install -y software-properties-common
-apt-add-repository -y ppa:ansible/ansible
-apt-get -y update
-apt-get install -y ansible git
+apt-get update
+apt-get install -y git
 cd /home/scrapbook/tutorial
 mkdir shared_volume
 docker pull virtuant/ansible-host-node:plus
@@ -13,3 +10,4 @@ docker run -d --name ansible --rm --network mynetwork -v $(pwd):/shared_volume v
 docker run -d --name target -p 8080 --rm --network mynetwork -v $(pwd):/shared_volume ubuntu:14.04 bash -c 'apt-get update; apt-get install -y openssh-server; service ssh start; while true; do sleep 60; echo keepalive; done'
 a() { docker exec -it ansible bash -c "echo 'PS1='\''ansible# '\' >> /root/.bashrc; bash"; }
 t() {  docker exec -it target bash -c "echo 'PS1='\''target# '\' >> /root/.bashrc; bash";  }
+echo "Ready!"
