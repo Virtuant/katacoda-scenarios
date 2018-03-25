dd370f3eeff930a8445de647fff9cc5f7fc74853
James B. Matthews <james30152@gmail.com>
2018-03-25 17:51:06 -0400

    add

--- jenkins/jenkins-build-docker-images/env-init.sh
+++ jenkins/jenkins-build-docker-images/env-init.sh
@@ -1,2 +1,3 @@
 #ssh root@host1 ""
-#ssh root@host1 "docker run -d -u root --name jenkins -p 8080:8080 -p 50000:50000 -v /root/jenkins:/var/jenkins_home jenkins/jenkins:lts"
+ssh root@host1 -y
+docker run -d -u root --name jenkins -p 8080:8080 -p 50000:50000 -v /root/jenkins:/var/jenkins_home jenkins/jenkins:lts
