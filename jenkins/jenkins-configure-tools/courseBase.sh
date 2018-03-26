b2bf31fe8492a1cdd3e67e1aff0d54e3e24455c9
David Norwood <davidn@virtuant.com>
2018-03-26 16:47:04 -0400

    Update courseBase.sh

--- jenkins/jenkins-configure-tools/courseBase.sh
+++ jenkins/jenkins-configure-tools/courseBase.sh
@@ -1,2 +1,3 @@
-ssh root@host1 -y
-docker run -d -u root --name jenkins -p 8080:8080 -p 50000:50000 -v /root/jenkins:/var/jenkins_home virtuant/jenkins-admin:v1
+# may delete this
+# ssh root@host1 -y
+# docker run -d -u root --name jenkins -p 8080:8080 -p 50000:50000 -v /root/jenkins:/var/jenkins_home virtuant/jenkins-admin:v1
