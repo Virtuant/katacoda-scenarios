847782ade6834a3cca090f565074667d712f3e0d
James B. Matthews <james30152@gmail.com>
2018-03-26 12:31:49 -0400

    added jenkins pull update

--- jenkins/jenkins-env-upgrade/courseBase.sh
+++ jenkins/jenkins-env-upgrade/courseBase.sh
@@ -1,6 +1,2 @@
-apt install update
-docker pull jenkins:2.46.1
-docker run -d -u root --name jenkins \
-    -p 8080:8080 -p 50000:50000 \
-    -v /root/jenkins:/var/jenkins_home \
-    jenkins:2.46.1
+ssh root@host1 -y
+docker run -d -u root --name jenkins -p 8080:8080 -p 50000:50000 -v /root/jenkins:/var/jenkins_home virtuant/jenkins-admin:v1
--- jenkins/jenkins-first/env-init.sh
+++ jenkins/jenkins-first/env-init.sh
@@ -1,5 +1,2 @@
-
-docker run -d -u root --name jenkins \
--p 8080:8080 -p 50000:50000 \
--v /root/jenkins:/var/jenkins_home \
-jenkins/jenkins:lts
+ssh root@host1 -y
+docker run -d -u root --name jenkins -p 8080:8080 -p 50000:50000 -v /root/jenkins:/var/jenkins_home virtuant/jenkins-admin:v1
