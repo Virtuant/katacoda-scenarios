2905825ff6f958b8a04330c3165e408312df9d17
James B. Matthews <james30152@gmail.com>
2018-03-27 15:03:06 -0400

    Merge branch 'master' of https://github.com/Virtuant/katacoda-scenarios

--- ansible/04-ansible-wordpress/env-init.sh
+++ ansible/04-ansible-wordpress/env-init.sh
@@ -27,3 +27,4 @@ docker pull ubuntu:14.04
 docker network create mynetwork
 docker run -d --name target -p 8080 --rm --network mynetwork ubuntu:14.04 bash -c 'apt-get update; apt-get install -y openssh-server; service ssh start; while true; do sleep 60; echo keepalive; done'
 t() {  docker exec -it target bash -c "echo 'PS1='\''target# '\' >> /root/.bashrc; bash";  }
+echo "Ready!"
