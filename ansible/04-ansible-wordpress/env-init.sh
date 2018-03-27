49503ea57e989511cf67289d37a005ac9cc53fa1
James B. Matthews <james30152@gmail.com>
2018-03-27 15:13:40 -0400

    add

--- ansible/04-ansible-wordpress/env-init.sh
+++ ansible/04-ansible-wordpress/env-init.sh
@@ -15,16 +15,16 @@
 # t() {  docker exec -it target bash -c "echo 'PS1='\''target# '\' >> /root/.bashrc; bash";  }
 
 # CORE INSTALLATION
-apt-get -y update
-apt-get install -y software-properties-common
-apt-add-repository -y ppa:ansible/ansible
+# apt-get -y update
+# apt-get install -y software-properties-common
+# apt-add-repository -y ppa:ansible/ansible
 apt-get -y update
 apt-get install -y ansible git
 cd /home/scrapbook/tutorial
 
 # DOCKER TARGET CONTAINER
-docker pull ubuntu:14.04
-docker network create mynetwork
-docker run -d --name target -p 8080 --rm --network mynetwork ubuntu:14.04 bash -c 'apt-get update; apt-get install -y openssh-server; service ssh start; while true; do sleep 60; echo keepalive; done'
-t() {  docker exec -it target bash -c "echo 'PS1='\''target# '\' >> /root/.bashrc; bash";  }
+# docker pull ubuntu:14.04
+# docker network create mynetwork
+# docker run -d --name target -p 8080 --rm --network mynetwork ubuntu:14.04 bash -c 'apt-get update; apt-get install -y openssh-server; service ssh start; while true; do sleep 60; echo keepalive; done'
+# t() {  docker exec -it target bash -c "echo 'PS1='\''target# '\' >> /root/.bashrc; bash";  }
 echo "Ready!"
