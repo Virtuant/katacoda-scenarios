f487bade93745ac9baf2f64c5eac8e07be46fb50
James B. Matthews <james30152@gmail.com>
2018-03-27 15:15:44 -0400

    add

--- ansible/04-ansible-wordpress/env-init.sh
+++ ansible/04-ansible-wordpress/env-init.sh
@@ -19,7 +19,7 @@
 # apt-get install -y software-properties-common
 # apt-add-repository -y ppa:ansible/ansible
 apt-get -y update
-apt-get install -y ansible git
+apt-get install -y git
 cd /home/scrapbook/tutorial
 
 # DOCKER TARGET CONTAINER
