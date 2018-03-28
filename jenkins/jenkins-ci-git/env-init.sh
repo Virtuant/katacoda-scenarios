20df73cc0b0eca56add502579dcd42532a3a5e2f
David Norwood <davidn@virtuant.com>
2018-03-28 10:25:20 -0400

    Update env-init.sh

--- jenkins/jenkins-ci-git/env-init.sh
+++ jenkins/jenkins-ci-git/env-init.sh
@@ -1,2 +1,9 @@
-ssh root@host1 -y
-docker run -d -u root --name jenkins -p 8080:8080 -p 50000:50000 -v /root/jenkins:/var/jenkins_home virtuant/jenkins-admin:v1
+docker run -d -u root --rm --name jenkins \
+    -p 8080:8080 -p 50000:50000 \
+    --entrypoint bash \
+    jenkins:2.46.2-alpine \
+    -c "tail -F /jenkins.log"
+docker exec -d jenkins \
+    bash -c 'git clone https://github.com/oveits/jenkins_home_alpine \
+        && export JENKINS_HOME=$(pwd)/jenkins_home_alpine \
+        && java -jar /usr/share/jenkins/jenkins.war 2>&1 1>/jenkins.log &'
