9e694b296b5c747b87a955f0d3cab796fb850cca
James B. Matthews <james30152@gmail.com>
2018-04-24 14:31:47 -0400

    add

--- ansible/04-ansible-wordpress/env-init.sh
+++ ansible/04-ansible-wordpress/env-init.sh
@@ -1,3 +1,8 @@
 ## CORE INSTALLATION
-# [code-here]
+apt-get -y update && \
+apt-get install -y software-properties-common && \
+apt-add-repository -y ppa:ansible/ansible && \
+apt-get -y update && \
+apt-get install -y ansible && \
+cd /home/scrapbook/tutorial
 echo "Ready!"
